22f1881d9ccd21cbcb3bf5e2c27b62117889cdfa
facunare <arechaga.facundoet36@gmail.com>
2023-03-07 19:13:06 -0300

    autentication

---
+++ autentication/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path, include
+from . import views
+
+from django.views.generic import TemplateView
+urlpatterns = [
+
+    path('signup/', views.signup, name='signup'),
+    path('signout/', views.signout, name='logout'),
+    # path('signin/', views.signin, name='signin'),
+]
---
+++ autentication/views.py
@@ -0,0 +1,55 @@
+from django.shortcuts import render
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib.auth.models import User
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import login, logout, authenticate
+
+# Create your views here.
+
+def signup(request):
+    if request.method == 'GET':
+        return render(request, 'signup.html',{
+            'form' : UserCreationForm
+        })
+    else:
+        if request.POST['password1'] == request.POST['password2']:
+            try:
+                user = User.objects.create_user(username=request.POST['username'], password=request.POST['password1'])
+                user.save()
+                return redirect('/signin/')
+            except:
+                
+                return render(request, 'signup.html',{
+                    'form' : UserCreationForm,
+                    'error': 'Usuario existente'
+                })
+        else:
+            return render(request, 'signup.html',{
+                'form' : UserCreationForm,
+                'error': 'Las contraseñas no coinciden'
+            })
+
+@login_required
+def signout(request):
+    logout(request)
+    return redirect('/')
+
+
+def signin(request):
+    if request.method == 'GET':
+
+        return render(request, 'signin.html',{
+            'form': AuthenticationForm
+        })
+    else:
+        user = authenticate(request, username=request.POST['username'], password=request.POST['password'])
+        if user is None:
+
+            return render(request, 'signin.html',{
+                'form': AuthenticationForm,
+                'error': "El usuario o contraseña es incorrecto"
+            })
+        else:
+            login(request, user)
+            return redirect('/globalFeed')
